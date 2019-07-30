419fee3764004602c36789fabf4648e9a917c76a
dtroode <davidkis113@gmail.com>
2019-07-30 23:00:47 +0300

    Add mobile and dark mode

--- scripts/main.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('/sw.js')
-           .then(function() { console.log('Прогуляйся по Сухуму без интернета!'); });
-}
