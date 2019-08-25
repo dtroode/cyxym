1bc77f12bc5e50f139ddea0ba2566bda60a79804
dtroode <davidkis113@gmail.com>
2019-08-25 19:13:56 +0300

    Design fixes

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,12 +1,12 @@
 const links = document.querySelectorAll("main nav a");
 
-links.forEach(link => {
-  link.addEventListener("click", function(event) {
-    event.preventDefault();
-    scrollTo(link.attributes.href.value);
-  });
-});
-
 function scrollTo(href) {
   document.querySelector(href).scrollIntoView({ behavior: "smooth" });
 }
+
+links.forEach(link => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    scrollTo(this.attributes.href.value);
+  });
+});
