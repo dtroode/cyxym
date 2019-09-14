45c23205b028a823ee0d0755ec58e2c23d65f5be
dtroode <davidkis113@gmail.com>
2019-09-14 14:35:18 +0300

    Change main en page

--- src/routes/en/index.svelte
+++ src/routes/en/index.svelte
@@ -1,5 +1,27 @@
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>CYXYM</title>
+  <meta
+    name="Description"
+    content="Sukhum is the long story, that starts from 6th century BC, its a lot of tourists, hotels and restaurants, its the honest people with own cuisine"
+  />
+  <meta itemprop="name" content="CYXYM" />
+  <meta
+    itemprop="description"
+    content="Sukhum is the long story, that starts from 6th century BC, its a lot of tourists, hotels and restaurants, its the honest people with own cuisine"
+  />
+  <meta property="og:url" content="https://cyxym.netlify.com/en" />
+  <meta property="og:title" content="CYXYM" />
+  <meta
+    property="og:description"
+    content="Sukhum is the long story, that starts from 6th century BC, its a lot of tourists, hotels and restaurants, its the honest people with own cuisine"
+  />
+  <meta name="twitter:url" content="https://cyxym.netlify.com/en" />
+  <meta name="twitter:title" content="CYXYM" />
+  <meta
+    name="twitter:description"
+    content="Sukhum is the long story, that starts from 6th century BC, its a lot of tourists, hotels and restaurants, its the honest people with own cuisine"
+  />
+  <meta name="apple-mobile-web-app-title" content="CYXYM" />
 </svelte:head>
 
 <h1>Cyxym</h1>
