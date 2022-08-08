3b1adad895a5eef6aab3c9482f024662ec69e57d
Валерий <70258211+ValeryVerkhoturov@users.noreply.github.com>
2022-08-08 21:31:38 +0300

    Change history headers

--- src/routes/history/index.svelte
+++ src/routes/history/index.svelte
@@ -2,12 +2,12 @@
   import { scrollTo } from "../../scroll.js";
 
   const content = [
-    { id: "#first", name: "-VI—I" },
-    { id: "#second", name: "I—VI" },
-    { id: "#third", name: "VI—X" },
-    { id: "#fourth", name: "X—XVII" },
-    { id: "#fifth", name: "XVII—XIX" },
-    { id: "#sixth", name: "XIX—XX" },
+    { id: "#first", name: "−VI–I" },
+    { id: "#second", name: "I–VI" },
+    { id: "#third", name: "VI–X" },
+    { id: "#fourth", name: "X–XVII" },
+    { id: "#fifth", name: "XVII–XIX" },
+    { id: "#sixth", name: "XIX–XX" },
     { id: "#seventh", name: "XX" },
   ];
 </script>
@@ -47,7 +47,7 @@
   <button href="{item.id}" on:click="{scrollTo}">{item.name}</button>
   {/each}
 </nav>
-<h2 id="first">-VI–I&numsp;в.</h2>
+<h2 id="first">−VI–I&numsp;в.</h2>
 <figure>
   <img
     src="images/history-1.jpg"
